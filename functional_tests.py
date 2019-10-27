cb68c50b0a2875041eedc40e9e04131fc00413ac
Tyler Merry <tymerry@gmail.com>
2019-10-26 19:18:30 -0600

    building a passing test

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,5 @@ from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path=r'.\geckodriver.exe')
 browser.get('http://localhost:8000')
-browser.close()
 
 assert 'Django' in browser.title
