d174ad0b2fb871ec2eae060039a2cc3ef5f04be4
Tyler Merry <tymerry@gmail.com>
2019-10-29 21:29:20 -0600

    we got a super basic list rolling

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,6 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!!!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
