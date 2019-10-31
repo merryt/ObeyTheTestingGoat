edd3d3b031d26f1128a1e2863d8ae31d71043381
Tyler Merry <tymerry@gmail.com>
2019-10-31 17:27:43 -0600

    ALMOST passing the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -29,9 +32,10 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
-        self.fail("current loc 1110")
+        self.fail("current loc 1352")
         self.fail("finish the test?")
 
 if __name__ == '__main__':
